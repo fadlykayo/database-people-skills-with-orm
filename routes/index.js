9f94df0e5abdb6984e41e1d9d66c4223f59c6ac6
Fadly Kayo <f_kayo@yahoo.com>
2017-02-05 19:03:31 +0700

    Add associations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+let models = require('../models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  models.User_skills.findAll({raw: true}).then(function (data) {
+    res.render('pages/index', {user_skills: data})
+  })
+})
 
-module.exports = router;
+router.get('/user', function (req, res, next) {
+  models.Users.findAll({raw: true}).then(function (data) {
+    res.render('pages/users', {users: data})
+  })
+})
+
+router.get('/skill', function (req, res, next) {
+  models.Skills.findAll({raw: true}).then(function (data) {
+    res.render('pages/skills', {skills: data})
+  })
+})
+
+module.exports = router
